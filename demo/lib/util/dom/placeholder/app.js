7be55adea36f0b696d32958c43cd7366880c9e60
caolvchong <caolvchong@gmail.com>
2013-09-21 22:52:24 +0800

    placeholder功能

---
+++ demo/lib/util/dom/placeholder/app.js
@@ -0,0 +1,8 @@
+define(function(require, exports, module) {
+    var $ = require('$');
+    var Placeholder = require('../../../../../js/lib/util/dom/placeholder');
+
+    $(function() {
+        Placeholder.render();
+    });
+});
--- js/lib/util/dom/placeholder.js
+++ js/lib/util/dom/placeholder.js
@@ -4,5 +4,66 @@
  * Time: 3:25 PM
  */
 define(function(require, exports, module) {
+    var $ = require('$');
+    var Position = require('./position');
+    var Inputor = require('./inputor');
 
+    var detect = (function() {
+        var isSupport = 0;
+        return function() {
+            if(isSupport === 0) {
+                isSupport = 'placeholder' in document.createElement('input');
+            }
+            return isSupport;
+        };
+    })();
+
+    var Placeholder = function(node, text) {
+        if(!detect()) {
+            node = $(node);
+            if(!node.data('placeholder')) {
+                text = text || node.attr('placeholder');
+                var p = $('<span class="placeholder">' + text + '</span>');
+                var h = node.outerHeight();
+                node.after(p);
+                p.css({
+                    color: '#bbb',
+                    height: h,
+                    lineHeight: h + 'px',
+                    paddingLeft: '9px'
+                });
+                Position.pin({
+                    element: p
+                }, {
+                    element: node
+                });
+
+                node.timer(function(text, input) {
+                    p[text.length ? 'hide' : 'show']();
+                });
+
+                p.bind('focus click', function() {
+                    node.focus();
+                });
+
+                if(node.val().length) {
+                    p.hide();
+                }
+
+                node.data('placeholder', true);
+            }
+        }
+    };
+
+    Placeholder.render = function() {
+        $('[placeholder]').placehoder();
+    };
+
+    $.fn.placehoder = function() {
+        this.each(function() {
+            Placeholder(this);
+        });
+    };
+
+    module.exports = Placeholder;
 });
