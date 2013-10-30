6305fa584656558ad1c1d3f77b0de315feae7f98
caolvchong <caolvchong@gmail.com>
2013-10-30 14:58:52 +0800

    配置更新

--- public/js/config.js
+++ public/js/config.js
@@ -1,17 +1,17 @@
 (function() {
     var version = '0.0.1'; // 线上部署版本
     var development = true; // 开关：true开发版本 false部署版本
-    var _ts = new Date().getTime();
+    var _ts = location.search.indexOf('use-cache') > -1 ? '' : new Date().getTime();
     var plugins = [];
     var map = [];
-    if(location.href.indexOf('development') > 0 || location.search.indexOf('seajs-debug') > 0) {
+    if(location.href.indexOf('development') > -1 || location.search.indexOf('seajs-debug') > -1) {
         development = true;
     }
     if(development) { // 开发模式
         var dist = 'public/js/dist/';
         var src = 'js/'
         map.push(function(url) {
-            if(url.indexOf(dist) > 0) {
+            if(url.indexOf(dist) > -1) {
                 url = url.replace(dist, src);
             }
             url += (url.indexOf('?') === -1 ? '?' : '&') + '_ts=' + _ts;
